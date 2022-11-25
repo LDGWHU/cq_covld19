2c27ecdb41b2c73545b81626359b71047ed1afba
LDGWHU <1905930871@qq.com>
2022-11-25 23:14:46 +0800

    Add files via upload

---
+++ precache-manifest.c930f8ea2a85c58282b52631086fc2bd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce65fbef91bff61ba1d1",
+    "url": "/css/app.67caa876.css"
+  },
+  {
+    "revision": "5193724ecc99c7898202",
+    "url": "/css/chunk-vendors.5ebd1c7f.css"
+  },
+  {
+    "revision": "4754c7c17b4db74d36d3eb823f7031e1",
+    "url": "/datas/point-2022-11-18.json"
+  },
+  {
+    "revision": "1803a825c01a1011a988bbf03fd530bd",
+    "url": "/datas/point-2022-11-19.json"
+  },
+  {
+    "revision": "bc2181e6aa73767eb63a2f492c606df0",
+    "url": "/datas/point-2022-11-19_.json"
+  },
+  {
+    "revision": "3806e0ba49eb5447e19a465e7b13eec5",
+    "url": "/datas/point-2022-11-20.json"
+  },
+  {
+    "revision": "4e3c50a0c83d8dc64974c7d75420866e",
+    "url": "/datas/point-2022-11-21.json"
+  },
+  {
+    "revision": "f0ce4cb2fe84cb7154fc86dba7cd2b0d",
+    "url": "/datas/point-2022-11-22.json"
+  },
+  {
+    "revision": "d5965574b949adfb409ba845814785a7",
+    "url": "/datas/point-2022-11-23.json"
+  },
+  {
+    "revision": "570c1b85b8ca7f5a98a8733ace55560f",
+    "url": "/datas/point-2022-11-24.json"
+  },
+  {
+    "revision": "7075be2e3417490da4b33bf2cdb65b21",
+    "url": "/datas/point-2022-11-24.json.qmd"
+  },
+  {
+    "revision": "c74d30ce8bceb62ca274dc1ba99a467c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce65fbef91bff61ba1d1",
+    "url": "/js/app.061a8fa7.js"
+  },
+  {
+    "revision": "5193724ecc99c7898202",
+    "url": "/js/chunk-vendors.a9be4748.js"
+  },
+  {
+    "revision": "9173271440408f8ad1aef32d86f5fdf1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
